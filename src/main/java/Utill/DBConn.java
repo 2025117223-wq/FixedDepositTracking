a68a3807faafad2f7dbe78241c7c404cbc38d6d3
MUHAMMAD NUR IMAN AIMAN BIN MOHD NOH <2025117223@student.uitm.edu.my>
2026-01-13 16:03:21 +0800

    Update DBConn.java

--- src/main/java/Utill/DBConn.java
+++ src/main/java/Utill/DBConn.java
@@ -6,7 +6,7 @@ public class DBConn {
 
     public static Connection getConnection() {
         // Set up database connection details
-        String url = "jdbc:postgresql://cee3ebbhveeoab.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dbu39dr34ej0dq"; // Replace with your Heroku host
+        String url = "jdbc:cee3ebbhveeoab.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com"; // Replace with your Heroku host
         String user = "u375elp7att1k5"; // Replace with your Heroku username
         String password = "p7dd455ccb3b6240ac52d8059200db4a112c7942d8084f4753cbff9186626a833"; // Replace with your Heroku password
         
