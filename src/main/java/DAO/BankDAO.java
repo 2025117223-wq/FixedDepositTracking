130ca0f254597dbfe88ab2565d1d91b34909b86a
MUHAMMAD NUR IMAN AIMAN BIN MOHD NOH <2025117223@student.uitm.edu.my>
2026-01-17 18:02:14 +0000

    initial commit

--- src/main/java/DAO/BankDAO.java
+++ src/main/java/DAO/BankDAO.java
@@ -1,4 +1,5 @@
 package DAO;
+
 import Utill.DBConn;
 import Bean.Bank;
 import java.sql.*;
@@ -8,39 +9,54 @@ import java.util.List;
 public class BankDAO {
 
     public boolean insertBank(Bank bank) throws SQLException {
-        String sql = "INSERT INTO Bank (bankName, bankAddress, bankPhone) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO bank (bankname, bankaddress, bankphone) VALUES (?, ?, ?)";
         try (Connection conn = DBConn.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
+
             ps.setString(1, bank.getBankName());
             ps.setString(2, bank.getBankAddress());
             ps.setString(3, bank.getBankPhone());
+
             return ps.executeUpdate() > 0;
         }
     }
 
     public List<Bank> getAllBanks() throws SQLException {
         List<Bank> banks = new ArrayList<>();
-        String sql = "SELECT * FROM Bank ORDER BY bankId DESC";
+        String sql = "SELECT bankid, bankname, bankaddress, bankphone FROM bank ORDER BY bankid DESC";
+
         try (Connection conn = DBConn.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql);
              ResultSet rs = ps.executeQuery()) {
+
             while (rs.next()) {
-                banks.add(new Bank(rs.getInt("bankId"), rs.getString("bankName"), 
-                          rs.getString("bankAddress"), rs.getString("bankPhone")));
+                banks.add(new Bank(
+                        rs.getInt("bankid"),
+                        rs.getString("bankname"),
+                        rs.getString("bankaddress"),
+                        rs.getString("bankphone")
+                ));
             }
         }
         return banks;
     }
 
     public Bank getBankById(int id) throws SQLException {
-        String sql = "SELECT * FROM Bank WHERE bankId = ?";
+        String sql = "SELECT bankid, bankname, bankaddress, bankphone FROM bank WHERE bankid = ?";
+
         try (Connection conn = DBConn.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
+
             ps.setInt(1, id);
+
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
-                    return new Bank(rs.getInt("bankId"), rs.getString("bankName"), 
-                                    rs.getString("bankAddress"), rs.getString("bankPhone"));
+                    return new Bank(
+                            rs.getInt("bankid"),
+                            rs.getString("bankname"),
+                            rs.getString("bankaddress"),
+                            rs.getString("bankphone")
+                    );
                 }
             }
         }
@@ -48,12 +64,15 @@ public class BankDAO {
     }
 
     public boolean updateBank(Bank bank) throws SQLException {
-        String sql = "UPDATE Bank SET bankAddress = ?, bankPhone = ? WHERE bankId = ?";
+        String sql = "UPDATE bank SET bankaddress = ?, bankphone = ? WHERE bankid = ?";
+
         try (Connection conn = DBConn.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
+
             ps.setString(1, bank.getBankAddress());
             ps.setString(2, bank.getBankPhone());
             ps.setInt(3, bank.getBankId());
+
             return ps.executeUpdate() > 0;
         }
     }
