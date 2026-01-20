3db7b878a8116016044e44f60a03dcc92cb17eaa
MUHAMMAD NUR IMAN AIMAN BIN MOHD NOH <2025117223@student.uitm.edu.my>
2026-01-20 18:09:19 +0000

    initial commit

---
+++ src/main/java/com/fd/FDTrackingApplication.java
@@ -0,0 +1,11 @@
+package com.fd; // Adjust to match your package structure
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdTrackingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FdTrackingApplication.class, args);
+    }
+}
---
+++ src/main/java/com/fd/dto/LoginResponse.java
@@ -0,0 +1,32 @@
+package com.fd.dto;
+
+public class LoginResponse {
+    private String token;
+    private String name;
+    private String role;
+
+    // Getters and Setters
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
